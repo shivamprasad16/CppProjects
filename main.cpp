05eb66060284dde1e218aa065d6db7a914348791
shivamprasad16 <epsshivam@gmail.com>
2026-05-27 17:17:35 +0530

    initialized the project

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+
+bool isPowerOfTwoShift(int n) {
+    // 1. A power of 2 must be greater than 0
+    // 2. __builtin_ctz(n) counts the number of trailing zeros (e.g., for 8 (1000), it's 3)
+    // 3. We shift 1 to the left by that amount (1 << 3 = 8). 
+    // If it equals the original n, it's a power of 2.
+    return (n > 0) && ((1 << __builtin_ctz(n)) == n);
+}
+
+int main() {
+    int n = 4;
+
+    cout << isPowerOfTwoShift(n);
+
+    
+
+   
+    return 0;
+}
+
